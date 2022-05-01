8de9b3d1a65f875e6844d819646695a7858ba3a2
Aa092667172 <ttaehee520@gmail.com.tw>
2022-05-01 22:12:22 +0800

    調整git結構

--- mall/src/main/java/com/kevin/mall/dao/ProductDao.java
+++
@@ -1,7 +0,0 @@
-package com.kevin.mall.dao;
-
-import com.kevin.mall.model.Product;
-
-public interface ProductDao {
-    Product getProductById(Integer productId);
-}
--- mall/src/main/java/com/kevin/mall/service/ProductService.java
+++
@@ -1,7 +0,0 @@
-package com.kevin.mall.service;
-
-import com.kevin.mall.model.Product;
-
-public interface ProductService {
-    Product getProductById(Integer productId);
-}
